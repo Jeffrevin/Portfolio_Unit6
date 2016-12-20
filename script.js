76be3854dea36838d8f15097b1e8478a4d3f668f
Kevin Zhong <jeffrevin@gmail.com>
2016-12-20 22:33:17 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-    //Write all your jQuery code here
-        $("button").click(function() {
+    $("#clickme").click(function() {
+        $("#clickme").html("jeffrevin@gmail.com");
+    });
+    
+    $("button").click(function() {
       $("#like").html("You like this project!");
     });
     
